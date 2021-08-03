49e4d3c1aa3d986f9e539336c2940ed479b40aed
angelsequeda <86447103+angelsequeda@users.noreply.github.com>
2021-08-03 17:26:31 -0500

    cors

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 app = express();
 const cors = require('cors');
+const corsOption = require('./middlewares');
 require('dotenv').config();
 
 //Middlewares globales
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cors())
+app.use(cors(corsOption));
+
+
+//Inicio de servidor
+
+app.listen(process.env.port, ()=> {
+
+    console.log('STATUS CORRECTO [SERVIDOR]');
+
+})
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,2 +1,18 @@
 require('dotenv').config();
 
+const corsOption = {
+
+    origin: function (origin,callback) {
+
+        if (process.env.LISTA_BLANCA.indexOf(origin) !== -1) {
+
+            callback (null,true);
+
+        }else {
+
+            callback(new Error('ERROR EN [CORSOPTIONS] (NO AUTORIZADO)') );
+        }
+    }
+}
+
+module.exports = corsOption;
