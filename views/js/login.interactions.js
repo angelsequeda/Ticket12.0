a24036cddaaf5243302ddba3fe4d3d7acca13ad8
angelsequeda <86447103+angelsequeda@users.noreply.github.com>
2021-08-04 14:11:52 -0500

    update html

--- views/js/login.interactions.js
+++ views/js/login.interactions.js
@@ -2,13 +2,12 @@
 
 document.getElementById('startSession').addEventListener('click', () => {
 
-    document.getElementById('secondivision').setAttribute('style','opacity: 1; align-self: flex-start');
+    document.getElementById('secondivision').setAttribute('style','opacity: 1');
     document.getElementById('firstdivision').setAttribute('style','opacity:0');
-    document.getElementById('thirddivision').setAttribute('style','opacity:0');
-    document.getElementById('completedivision').setAttribute('style','display: flex; flex-direction: column');
+    document.getElementById('completedivision').setAttribute('style','display: flex; flex-direction: column-reverse');
     document.getElementById('startSession').setAttribute('style','opacity:1');
     document.getElementById('registerNewUser').setAttribute('style','opacity:0.5');
-    document.getElementById('changePassword').setAttribute('style','opacity:0.5');
+
 
 })
 
@@ -17,9 +16,12 @@ document.getElementById('registerNewUser').addEventListener('click', () => {
 
     document.getElementById('secondivision').setAttribute('style','opacity: 0');
     document.getElementById('firstdivision').setAttribute('style','opacity:1');
-    document.getElementById('thirddivision').setAttribute('style','opacity:0');
     document.getElementById('completedivision').setAttribute('style','display: flex; flex-direction: column');
     document.getElementById('startSession').setAttribute('style','opacity:0.5');
-    document.getElementById('changePassword').setAttribute('style','opacity:0.5');
     document.getElementById('registerNewUser').setAttribute('style','opacity:1');
+})
+
+
+document.getElementById('changePassword').addEventListener('click' , ()=> {
+    document.getElementById('completedivision').innerHTML = document.getElementById('thirddivision').innerHTML
 })
