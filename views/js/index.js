fd41a8c9e512b5d4e86e436084393ef6df856ef6
angelsequeda <86447103+angelsequeda@users.noreply.github.com>
2021-08-05 17:19:32 -0500

    update
    He comenzado a trabajar con la tabla de creación de nuevo presupuesto

--- views/js/index.js
+++ views/js/index.js
@@ -53,8 +53,8 @@ export class Renderizer {
 
         this.button = document.createElement('button');
     }
-
-    static async renderIndex(data) {
+    
+    static renderIndex(data) {
         let table = document.getElementById('bodyindex');
         data.forEach(element1 => {
             let file = document.createElement('tr');
@@ -64,7 +64,32 @@ export class Renderizer {
                 newcolumn.textContent = element1[element2];
                 file.appendChild(newcolumn);
             });
+            let newcolumn1 = document.createElement('td');
+            newcolumn1.innerHTML = `<button class ='btn btn-success' id='${element1.id_presupuesto}send'>Enviar</button>`;
+            let newcolumn2 = document.createElement('td');
+            newcolumn2.innerHTML = `<button class ='btn btn-info' id='${element1.id_presupuesto}edit'>Editar</button>`;
+            let newcolumn3 = document.createElement('td');
+            newcolumn3.innerHTML = `<button class ='btn btn-danger' id='${element1.id_presupuesto}delete'>Eliminar</button>`
+            file.appendChild(newcolumn1);
+            file.appendChild(newcolumn2);
+            file.appendChild(newcolumn3);
             table.appendChild(file);
+        });
+    }
+
+    static activeButtonsIndex(data) {
+
+        data.forEach(element => {
+
+            document.getElementById(element.id_presupuesto+'send').addEventListener('click', ()=> {
+                console.log(element.id_presupuesto);
+            });
+            document.getElementById(element.id_presupuesto+'edit').addEventListener('click', ()=> {
+                console.log(element.id_presupuesto);
+            });
+            document.getElementById(element.id_presupuesto+'delete').addEventListener('click', ()=> {
+                console.log(element.id_presupuesto);
+            })
         });
     }
 }
--- views/js/indexpresupuestos.js
+++ views/js/indexpresupuestos.js
@@ -2,4 +2,6 @@ import { Budget } from "./classes.js";
 import { Renderizer } from "./index.js";
 
 let result = await Budget.searchAllBudgets();
+console.log(result);
 Renderizer.renderIndex(result);
+Renderizer.activeButtonsIndex(result);
