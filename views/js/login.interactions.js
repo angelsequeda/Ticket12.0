a49d7a12ade4aa2175338086a418cac5c101d6d0
angelsequeda <86447103+angelsequeda@users.noreply.github.com>
2021-08-04 23:24:38 -0500

    Update login.interactions.js

--- views/js/login.interactions.js
+++ views/js/login.interactions.js
@@ -1,31 +1,3 @@
-
-/*
-document.getElementById('startSession').addEventListener('click', () => {
-
-    document.getElementById('secondivision').setAttribute('style','opacity: 1');
-    document.getElementById('firstdivision').setAttribute('style','opacity:0');
-    document.getElementById('completedivision').setAttribute('style','display: flex; flex-direction: column-reverse');
-    document.getElementById('startSession').setAttribute('style','opacity:1');
-    document.getElementById('registerNewUser').setAttribute('style','opacity:0.5');
-
-
-})
-
-
-document.getElementById('registerNewUser').addEventListener('click', () => {
-
-    document.getElementById('secondivision').setAttribute('style','opacity: 0');
-    document.getElementById('firstdivision').setAttribute('style','opacity:1');
-    document.getElementById('completedivision').setAttribute('style','display: flex; flex-direction: column');
-    document.getElementById('startSession').setAttribute('style','opacity:0.5');
-    document.getElementById('registerNewUser').setAttribute('style','opacity:1');
-})
-
-
-document.getElementById('changePassword').addEventListener('click' , ()=> {
-    document.getElementById('completedivision').innerHTML = document.getElementById('thirddivision').innerHTML
-})*/
-
 $(document).ready(function() {
 
     $(".registerdivision").hide();
