eb5704cff8fe51ea01ba1f2fd338ef578ef3bffb
angelsequeda <86447103+angelsequeda@users.noreply.github.com>
2021-08-04 23:18:22 -0500

    update html
    Ahora funcionan las tabs

--- views/js/login.interactions.js
+++ views/js/login.interactions.js
@@ -1,3 +1,53 @@
-$(document).ready(function(){
-    console.log('funcionando')
+
+/*
+document.getElementById('startSession').addEventListener('click', () => {
+
+    document.getElementById('secondivision').setAttribute('style','opacity: 1');
+    document.getElementById('firstdivision').setAttribute('style','opacity:0');
+    document.getElementById('completedivision').setAttribute('style','display: flex; flex-direction: column-reverse');
+    document.getElementById('startSession').setAttribute('style','opacity:1');
+    document.getElementById('registerNewUser').setAttribute('style','opacity:0.5');
+
+
+})
+
+
+document.getElementById('registerNewUser').addEventListener('click', () => {
+
+    document.getElementById('secondivision').setAttribute('style','opacity: 0');
+    document.getElementById('firstdivision').setAttribute('style','opacity:1');
+    document.getElementById('completedivision').setAttribute('style','display: flex; flex-direction: column');
+    document.getElementById('startSession').setAttribute('style','opacity:0.5');
+    document.getElementById('registerNewUser').setAttribute('style','opacity:1');
+})
+
+
+document.getElementById('changePassword').addEventListener('click' , ()=> {
+    document.getElementById('completedivision').innerHTML = document.getElementById('thirddivision').innerHTML
+})*/
+
+$(document).ready(function() {
+
+    $(".registerdivision").hide();
+    $(".changerdivision").hide();
+
+    $("#startSession").click(function() {
+
+        $(".logindivision").fadeIn();
+        $(".registerdivision").fadeOut();
+        $(".changerdivision").fadeOut();
+    })
+    $("#registerUser").click(function() {
+
+        $(".registerdivision").fadeIn();
+        $(".logindivision").fadeOut();
+        $(".changerdivision").fadeOut();
+    })
+    $("#passwordChange").click( function() {
+
+        $(".registerdivision").fadeOut();
+        $(".logindivision").fadeOut();
+        $(".changerdivision").fadeIn();
+
+    })
 })
